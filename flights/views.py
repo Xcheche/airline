3cdb3a2b41ae6878d59f074fb911d2e4f57d7c10
xcheche <chekwubechukwu23@gmail.com>
2024-04-15 21:07:59 +0100

    passenger model added

--- flights/views.py
+++ flights/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from .models import Flight
+import datetime
 # Create your views here.
 
 
 def index(request):
     return render(request, 'flights/index.html', {
         'flights': Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    date = datetime.datetime.now()
+    flight = Flight.objects.get(pk=flight_id)
+
+    return render(request,'flights/flight.html',{
+        'flight':flight,'date':date
     })
